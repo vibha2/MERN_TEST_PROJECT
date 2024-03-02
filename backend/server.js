d18de0b7caa2b8873a04e09ab00b585bcc481974
vibha2 <juhisahu2203@gmail.com>
2024-03-03 01:17:38 +0530

    made build

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const database = require('./config/database');
+
+
+// middlewares
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+//to make build
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", function(req, res){
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+})
+
+app.listen(PORT, () => {
+    console.log("App is running on "+ PORT);
+})
